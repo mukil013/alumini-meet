abd1c2a58f774a123565e0b1a7bc545849c77784
mukil013 <mukilmukil1947@gmail.com>
2025-03-17 00:15:24 +0530

    added edit route

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -14,65 +14,80 @@ import UserManagement from "./pages/admin/UserManagement.tsx";
 import AdminHome from "./pages/admin/AdminHome.tsx";
 import PlacementInfo from "./pages/admin/PlacementInfo.tsx";
 import Refferral from "./pages/Refferral.tsx";
+import EditEvent from "./pages/admin/EditEvents.tsx";
 
-const isAuth = !!localStorage.getItem("user") 
-const isAdmin = JSON.parse(localStorage.getItem("user")!) || null
+const isAuth = !!localStorage.getItem("user");
+const isAdmin = JSON.parse(localStorage.getItem("user")!) || null;
 
 const router = Bro([
   {
-    path: '/',
-    element: isAuth ? isAdmin.role === 'admin' ? <AdminHome /> : <Home /> : <Login />, 
+    path: "/",
+    element: isAuth ? (
+      isAdmin.role === "admin" ? (
+        <AdminHome />
+      ) : (
+        <Home />
+      )
+    ) : (
+      <Login />
+    ),
   },
   {
-    path: '/userManagement',
-    element: <UserManagement/>
+    path: "/userManagement",
+    element: <UserManagement />,
   },
   {
-    path: '/placement-info',
-    element: <PlacementInfo />
+    path: "/placement-info",
+    element: <PlacementInfo />,
   },
   {
-    path: '/register',
+    path: "/edit-event",
+    element: <EditEvent />,
+  },
+  {
+    path: "/register",
     element: <Register />,
   },
   {
-    path: '/login',
+    path: "/login",
     element: <Login />,
   },
   {
-    path: '/home', 
+    path: "/home",
     element: <Home />,
     children: [
       {
-        path: 'batches',
+        path: "batches",
         element: <Batches />,
       },
       {
-        path: 'profile', 
+        path: "profile",
         element: <Profile />,
       },
       {
-        path: 'Refferral',
-        element: <Refferral />
+        path: "Refferral",
+        element: <Refferral />,
       },
       {
-        path: 'placements',
-        element: <Placement />
+        path: "placements",
+        element: <Placement />,
       },
       {
-        path: 'mentorship',
-        element: <Mentorship />
+        path: "mentorship",
+        element: <Mentorship />,
       },
       {
-        path: 'projects',
-        element: <Projects />
+        path: "projects",
+        element: <Projects />,
       },
       {
-        path: 'event',
-        element: <Events />
-      }
+        path: "event",
+        element: <Events />,
+      },
     ],
   },
 ]);
 
-createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
