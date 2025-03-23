310596e26ccdfe0ad3870ff8501c5e657ff15134
mukil013~ <mukilmukil1947@gmail.com>
2025-03-23 18:19:13 +0530

    common navbar updated

--- front-end/src/components/Navbar.tsx
+++ front-end/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import "./style/Navbar.css";
 
 export default function Navbar() {
   const navigate = useNavigate();
-  const location = useLocation(); // Get the current location
+  const location = useLocation();
 
   const handleLogout = () => {
     localStorage.removeItem("user");
@@ -12,10 +12,11 @@ export default function Navbar() {
     navigate("/login"); // Redirect to the login page after logout
   };
 
-  // Function to check if a link is active
   const isActive = (path: string) => {
     return location.pathname === path;
   };
+
+  isActive("/home");
 
   return (
     <nav className="common-nav">
@@ -24,9 +25,7 @@ export default function Navbar() {
         <li className="common-nav-list-items">
           <Link
             to="/home/"
-            className={
-              isActive("/home/") ? "active" : "" + "common-nav-link"
-            }
+            className={isActive("/home/") ? "active" : "" + "common-nav-link"}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -188,6 +187,7 @@ export default function Navbar() {
             <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z" />
           </svg>
         </Link>
+        <hr className="common-nav-ruler" />
         <button className="common-logout" onClick={handleLogout} title="logout">
           <svg
             xmlns="http://www.w3.org/2000/svg"
