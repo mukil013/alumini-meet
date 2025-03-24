ba9a65e0a86349b8999a48ba788601585553a3db
mukil013~ <mukilmukil1947@gmail.com>
2025-03-25 01:17:36 +0530

    added project with image(QR)

--- backend/routes/projectRoute.js
+++ backend/routes/projectRoute.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const {addProject, editProject, deleteProject, getAllProject, getUserProject} = require('../controller/projectController');
+const {handleFileUpload,addProject, editProject, deleteProject, getAllProject, getUserProject, getProjectImage} = require('../controller/projectController');
 const router = express.Router();
 
-// http://localhost:8000/project/getAllProject
+router.post('/addProject/:userId', handleFileUpload, addProject);
 router.get('/getAllProjects', getAllProject);
-// http://localhost:8000/project/addProject/user id
-router.post('/addProject/:id', addProject);
 // http://localhost:8000/project/deleteProject/ project id
-router.delete('/deleteProject/:id', deleteProject);
+router.delete('/deleteProject/:projectId', deleteProject);
 // http://localhost:8000/project/editProject/ project id
-router.patch('/editProject/:id', editProject);
+router.patch('/editProject/:projectId', editProject);
 // http://localhost:8000/project/getUserProject/ user id
-router.get('/getUserProject/:id', getUserProject);
+router.get('/getUserProject/:userId', getUserProject);
+// GET project image by project ID 
+router.get('/projectImage/:projectId', getProjectImage);
 
 module.exports = router;
